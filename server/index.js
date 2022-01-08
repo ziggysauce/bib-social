f7e52e4f84b8fcfcf3de2e22b4f2d1c05a9ac8ed
ziggysauce <dtn3423@gmail.com>
2022-01-08 00:00:57 -0500

    eslintrc; remove base route

--- server/index.js
+++ server/index.js
@@ -13,9 +13,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routing
-app.get('/', async (req, res) => {
-  res.status(200).json({ data: 'Server is connected!' });
-});
 app.use(routes.router);
 
 // Serve static files from express backend.
