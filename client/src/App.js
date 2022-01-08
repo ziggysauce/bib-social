80fceb890e697c8d9ed800400b018e2625fda1d3
ziggysauce <dtn3423@gmail.com>
2022-01-07 23:52:38 -0500

    basic react implementation

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/no-unused-state */
+
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Basic create-react-app setup.
+      </div>
+    );
+  }
+}
+
+export default App;
