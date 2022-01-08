c3925c4c6f200ff93c704ecbe0c0bf39c5bb3aea
ziggysauce <dtn3423@gmail.com>
2022-01-08 16:12:08 -0500

    react stuff is working

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     es2021: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-base', 'react-app', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,50 @@
-/* eslint-disable react/no-unused-state */
-
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      hasPermission: false,
+      password: '',
+      error: false,
+    };
+    this.onHandleInputKeyup = this.onHandleInputKeyup.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onHandleInputKeyup(e) {
+    this.setState({ password: e.target.value, error: false });
+  }
+
+  onSubmit() {
+    if(this.state.password === 'besadetroit2021') {
+      this.setState({ hasPermission: true });
+    } else {
+      this.setState({ error: true });
+    }
   }
 
   render() {
-    return <div>Basic create-react-app setup!</div>;
+    const { hasPermission, error } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          {hasPermission && <h1>Access granted!</h1>}
+          {!hasPermission &&
+            <div>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                This site is under maintenance. Pleae enter the password to continue.
+              </p>
+              <input onKeyUp={this.onHandleInputKeyup} type="text" placeholder="Enter the password..." />
+              <button onClick={this.onSubmit} >Submit</button>
+              {error && <p>Sorry that password is incorrect.</p>}
+            </div>}
+        </header>
+      </div>
+    );
   }
 }
 
