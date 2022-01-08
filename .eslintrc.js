7bdebdf0479503f9f60bb0394be22b4e9dc4e12d
ziggysauce <dtn3423@gmail.com>
2022-01-08 16:26:13 -0500

    bootstrap and classnames

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,13 @@ module.exports = {
   env: {
     es2021: true,
   },
-  extends: ['airbnb-base', 'react-app', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'react-app',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
